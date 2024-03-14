1ab4a2710af203259719d64637470d132cbc86de
blackmarllbor0 <3100194@gmail.com>
2024-03-14 18:06:34 +0300

    feat: added a dir to the migration, and rebuilt the table structure

---
+++ migrations/000002_public.down.sql
@@ -0,0 +1,16 @@
+drop index if exists "public".idx_public_search_settings_gender;
+drop index if exists "public".idx_public_search_settings_currency_id;
+drop index if exists "public".idx_public_search_settings_city_id;
+drop index if exists "public".idx_public_search_settings_country_id;
+drop index if exists "public".idx_public_search_settings_nationality_id;
+drop index if exists "public".idx_public_search_settings_user_id;
+drop index if exists "public".idx_public_users_hash_user_id;
+drop index if exists "public".idx_public_users_gender;
+drop index if exists "public".idx_public_users_nationality_id;
+drop index if exists "public".idx_public_users_id;
+
+drop table if exists "public"."users";
+drop table if exists "public"."users_hash";
+drop table if exists "public"."search_settings";
+
+drop schema if exists "public";
